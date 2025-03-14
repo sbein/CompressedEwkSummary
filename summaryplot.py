b788ca24b2ec7db4ae6971f21fc1a0afa48d5224
Peter Eduard Meiring <peter.eduard.meiring@cern.ch>
2025-03-14 16:28:48 +0100

    add exo23017 higgsino limits

--- summaryplot.py
+++ summaryplot.py
@@ -5,13 +5,14 @@ import os
 tl = ROOT.TLatex()
 tl.SetNDC()
 
-doHLLHC = True
+doHLLHC = False
     
 def main():
     analyses = {}
     analyses['SUS-21-006'] = ['results_sus_21_006/PureHiggsino_DTRun2_results.root', 'Exp_PureHiggsino_DTRun2', 'Obs_PureHiggsino_DTRun2', ROOT.kRed]
     analyses['SUS-24-012'] = ['results_sus_24_012/PureHiggsino_SDPRun2_results.root', 'Exp_PureHiggsino_SDPRun2', 'Obs_PureHiggsino_SDPRun2', ROOT.kBlue]
     analyses['SUS-24-003'] = ['results_sus_24_003/PureHiggsino_spdl_Run2comb_results.root', 'Exp_PureHiggsino_spdl_comb', 'Obs_PureHiggsino_spdl_comb', ROOT.kGreen + 2]
+    analyses['EXO-23-017'] = ['results_exo_23_017/h2lim_20250226_Hino_neg_allEEMM_neg_0.0_log_smooth1k5a_dMc1n1.root', 'limitGraph_0', 'limitGraph_obs', ROOT.kMagenta + 2]
     cadis = list(analyses.keys())
     cadis.reverse()
     if doHLLHC:
@@ -23,17 +24,18 @@ def main():
     canvas = ROOT.TCanvas("c1", "SUSY EW Summary Plot", 800, 600)
     print(canvas.GetTopMargin(), canvas.GetBottomMargin())
     canvas.SetBottomMargin(0.15)
-    canvas.SetTopMargin(0.05)
+    canvas.SetTopMargin(1.3)
     
     mg = ROOT.TMultiGraph()
-    legend = ROOT.TLegend(0.61, 0.63-0.07*(len(analyses)-3), 0.88, 0.88)
+    legend = ROOT.TLegend(0.61, 0.28-0.07*(len(analyses)-3), 0.88, 0.53)
     legend.SetBorderSize(0)
     legend.SetFillStyle(0)
     basefile = ROOT.TFile.Open('results_sus_24_003/PureHiggsino_SoftPromptRun2_18Nov2024HLLHCXSEC.root')
     #else: basefile = ROOT.TFile.Open('results_sus_24_003/PureHiggsino_SoftPromptRun2_25Nov2024Observed4thabaseXSEC.root')
     base_hist = basefile.Get('basehist')
     base_hist.GetYaxis().SetRangeUser(0.135,4.0)
-    base_hist.GetXaxis().SetRangeUser(99,210)    
+    base_hist.GetXaxis().SetRangeUser(99,250)
+    base_hist.GetYaxis().SetTitleOffset(0.8)
     if doHLLHC: 
         #base_hist.GetYaxis().SetRangeUser(0.135,4.3)
         base_hist.GetXaxis().SetRangeUser(99,340)   
@@ -87,21 +89,21 @@ def stamp(lumi='138', datamc_ = 'data', showlumi = True, WorkInProgress = False)
     datamc = datamc_.lower()
     tl.SetTextFont(cmsTextFont)
     tl.SetTextSize(0.98*tl.GetTextSize())
-    tl.DrawLatex(0.12,0.9, 'CMS')
+    tl.DrawLatex(0.12,0.91, 'CMS')
     tl.SetTextFont(extraTextFont)
     tl.SetTextSize(1.0/0.98*tl.GetTextSize())
     xlab = 0.2
     if ('mc' in datamc): thing = 'simulation'
     else: thing = 'preliminary'
-    if WorkInProgress: tl.DrawLatex(xlab,0.9, ' internal')
-    else: tl.DrawLatex(xlab,0.9, thing)
+    if WorkInProgress: tl.DrawLatex(xlab,0.91, ' internal')
+    else: tl.DrawLatex(xlab,0.91, thing)
     tl.SetTextFont(regularfont)
     tl.SetTextSize(0.81*tl.GetTextSize())    
     thingy = ''
     if showlumi: thingy+=str(lumi)+' fb^{-1} '+'(13 TeV)'
     xthing = 0.667
     if not showlumi: xthing+=0.13
-    tl.DrawLatex(xthing,0.9,thingy)
+    tl.DrawLatex(xthing,0.91,thingy)
     tl.SetTextSize(1.0/0.81*tl.GetTextSize())
     
 main()
