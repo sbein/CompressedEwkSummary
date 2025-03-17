23e887b529fad4ed925f93722371c0fd9ba9fbb7
botta <cristina.botta@cern.ch>
2025-03-17 15:19:52 +0100

    Add dirty trick to fill non-closed areas

--- summaryplot.py
+++ summaryplot.py
@@ -1,6 +1,7 @@
 import ROOT
 ROOT.gROOT.SetBatch(1)
 import os
+from array import array
 
 tl = ROOT.TLatex()
 tl.SetNDC()
@@ -10,8 +11,8 @@ drawRadidativeBound = True
     
 def main():
     analyses = {}
-    analyses['Disappearing Tracks'] = ['results_sus_21_006/PureHiggsino_DTRun2_results.root', 'Exp_PureHiggsino_DTRun2', 'Obs_PureHiggsino_DTRun2', ROOT.kViolet-1]#ROOT.kSpring+2
-    analyses['Isolated soft Track'] = ['results_sus_24_012/PureHiggsino_SDPRun2_results.root', 'Exp_PureHiggsino_SDPRun2', 'Obs_PureHiggsino_SDPRun2', ROOT.kGreen-1]# ROOT.kOrange-1
+    analyses['Disappearing Tracks'] = ['results_sus_21_006/PureHiggsino_DTRun2_results.root', 'Exp_PureHiggsino_DTRun2', 'Obs_PureHiggsino_DTRun2', ROOT.kSpring+2]#ROOT.kViolet-1 kSpring+2
+    analyses['Isolated soft Track'] = ['results_sus_24_012/PureHiggsino_SDPRun2_results.root', 'Exp_PureHiggsino_SDPRun2', 'Obs_PureHiggsino_SDPRun2', ROOT.kOrange-1]#ROOT.kGreen-1 kOrange-1
     #analyses['SUS-24-003'] = ['results_sus_24_003/PureHiggsino_spdl_Run2comb_results.root', 'Exp_PureHiggsino_spdl_comb', 'Obs_PureHiggsino_spdl_comb', ROOT.kRed]
     analyses['Soft 2l and 3l'] = ['results_exo_23_017/h2lim_20250226_Hino_neg_allEEMM_neg_0.0_log_smooth1k5a_dMc1n1.root', 'limitGraph_0', 'limitGraph_obs', ROOT.kAzure-9]
     analysis_names = list(analyses.keys())
@@ -28,11 +29,12 @@ def main():
     canvas.SetTopMargin(1.3)
     
     mg = ROOT.TMultiGraph()
-    legend0 = ROOT.TLegend(0.15, 0.70, 0.45, 0.95)
-    legend0.SetHeader("#splitline{#bf{pp #rightarrow #chi^{0}_{2}#chi^{#pm}_{1} #chi^{0}_{2}#chi^{#mp}_{1} #chi^{+}_{1}#chi^{-}_{1} #chi^{#mp}_{1}#chi^{0}_{1} (Higgsino)}}{#bf{m(#chi^{0}_{2}) = m(#chi^{0}_{1}) + 2#Delta m(#chi^{#pm}_{1}, #chi^{0}_{1})}}","C")
+    legend0 = ROOT.TLegend(0.12, 0.70, 0.20, 0.88)
+    legend0.SetHeader("#splitline{#bf{pp#rightarrow#chi^{0}_{2}#chi^{#pm}_{1} #chi^{0}_{2}#chi^{#mp}_{1} #chi^{+}_{1}#chi^{-}_{1} #chi^{#mp}_{1}#chi^{0}_{1} (Higgsino)}}{#bf{m(#chi^{0}_{2}) = m(#chi^{0}_{1}) + 2#Delta m(#chi^{#pm}_{1},#chi^{0}_{1})}}","L")
     legend0.SetBorderSize(0)
     legend0.SetFillStyle(0)
     legend0.SetTextSize(0.03)
+    legend0.SetTextFont(62)  # 62 = Helvetica grassetto
     dummyObs = ROOT.TH1F("dummyObs","",1,0,1)
     dummyObs.SetLineStyle(1)          
     dummyObs.SetLineColor(ROOT.kBlack)  # black line
@@ -68,6 +70,7 @@ def main():
     base_hist.GetXaxis().SetTitle('m_{#tilde{#chi}^{#pm}_{1}} [GeV]')
     base_hist.GetYaxis().SetTitle('#Deltam(#tilde{#chi}^{#pm}_{1},#tilde{#chi}^{0}_{1}) [GeV]')
     print(base_hist.GetXaxis().GetTitle(), base_hist.GetYaxis().GetTitle())
+    first = True  # flag per il primo elemento
     for analysis_name in analysis_names:
         analysis = analyses[analysis_name]
         path, exp, obj, color = analysis
@@ -83,28 +86,64 @@ def main():
         else: 
             gobs = file.Get(obj)
             gobs.SetLineColor(color)
-            #gobs.SetLineStyle(1)
+            #gobs.SetLineStyle(1)   
             gobs.SetLineWidth(3)
             gobs.SetFillColorAlpha(color, 0.35)
-            mg.Add(gobs, "LF")
-            #legend.AddEntry(gobs, "%s Observed" % analysis_name, "l")
-            legend.AddEntry(gobs, "%s" % analysis_name, "f")
-        file.Close()
+            # a dirty way to fill the are for the first analysis curve
+            if first:
+                N = gobs.GetN()
+                gobsFilled = ROOT.TGraph(N+2)
+                for j in range(N):
+                    x_arr = array('d', [0.])
+                    y_arr = array('d', [0.])
+                    gobs.GetPoint(j, x_arr, y_arr)
+                    gobsFilled.SetPoint(j, x_arr[0], y_arr[0])
+                # Add two points to close the area
+                x_arr = array('d', [0.])
+                y_arr = array('d', [0.])
+                gobs.GetPoint(N-1, x_arr, y_arr)
+                baseline = 0.0  
+                gobsFilled.SetPoint(N, x_arr[0], baseline)
+                x_arr = array('d', [0.])
+                y_arr = array('d', [0.])
+                gobs.GetPoint(0, x_arr, y_arr)
+                gobsFilled.SetPoint(N+1, x_arr[0], baseline)
+                gobsFilled.SetLineColor(color)
+                gobsFilled.SetLineWidth(3)
+                gobsFilled.SetFillColorAlpha(color, 0.35)
+                gobsFilled.SetFillStyle(1001)
+                mg.Add(gobsFilled, "LF")
+                legend.AddEntry(gobsFilled, "%s" % analysis_name, "f")
+            else:
+                mg.Add(gobs, "LF")
+                #legend.AddEntry(gobs, "%s Observed" % analysis_name, "l")
+                legend.AddEntry(gobs, "%s" % analysis_name, "f")
+    file.Close()
     canvas.cd()
     canvas.SetLogy()
     base_hist.Draw()
+    # needed for change ranges coherently on the MultiGraph
+    #mg.SetMinimum(0.2)
+    #mg.SetMaximum(50.0)
+    #mg.GetXaxis().SetLimits(100,250)  
+    #mg.GetYaxis().SetNdivisions(505)
+    #mg.GetYaxis().SetTitleOffset(0.8)
+    #mg.GetYaxis().SetMoreLogLabels(True)
+    #mg.GetYaxis().SetNoExponent(True)
+    #mg.GetXaxis().SetTitle('m_{#tilde{#chi}^{#pm}_{1}} [GeV]')
+    #mg.GetYaxis().SetTitle('#Deltam(#tilde{#chi}^{#pm}_{1},#tilde{#chi}^{0}_{1}) [GeV]')
+    #mg.Draw("AL")
     mg.Draw("L")
     legend0.Draw()
     legend00.Draw() 
     legend.Draw()
-    #canvas.SetLogy()
     #canvas.SetGrid()
     if drawRadidativeBound:
         fsatoshi = ROOT.TFile('auxiliary/SatoshiDmChipmChi10.root')
         dmrad_chipm_chi10 = fsatoshi.Get('dmrad_chipm_chi10')
-        dmrad_chipm_chi10.SetLineColor(ROOT.kOrange+2)
+        dmrad_chipm_chi10.SetLineColor(ROOT.kRed) #kOrange+2
         dmrad_chipm_chi10.SetLineWidth(2)
-        dmrad_chipm_chi10.SetLineStyle(ROOT.kDashed)        
+        dmrad_chipm_chi10.SetLineStyle(ROOT.kDotted)        
         dmrad_chipm_chi10.Draw('same')
         legend.AddEntry(dmrad_chipm_chi10, "Lower bound (theory)", "l")
     
