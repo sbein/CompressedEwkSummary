b3822c858738c1a328c9388344e7f0645188dbf2
sbein <Samuel.bein@gmail.com>
2025-03-17 09:10:06 +0100

    sleepless nights

--- summaryplot.py
+++ summaryplot.py
@@ -6,18 +6,19 @@ tl = ROOT.TLatex()
 tl.SetNDC()
 
 doHLLHC = False
+drawRadidativeBound = True
     
 def main():
     analyses = {}
-    analyses['Disappearing Tracks'] = ['results_sus_21_006/PureHiggsino_DTRun2_results.root', 'Exp_PureHiggsino_DTRun2', 'Obs_PureHiggsino_DTRun2', ROOT.kSpring+2]
-    analyses['Isolated soft Track'] = ['results_sus_24_012/PureHiggsino_SDPRun2_results.root', 'Exp_PureHiggsino_SDPRun2', 'Obs_PureHiggsino_SDPRun2', ROOT.kOrange-1]
+    analyses['Disappearing Tracks'] = ['results_sus_21_006/PureHiggsino_DTRun2_results.root', 'Exp_PureHiggsino_DTRun2', 'Obs_PureHiggsino_DTRun2', ROOT.kViolet-1]#ROOT.kSpring+2
+    analyses['Isolated soft Track'] = ['results_sus_24_012/PureHiggsino_SDPRun2_results.root', 'Exp_PureHiggsino_SDPRun2', 'Obs_PureHiggsino_SDPRun2', ROOT.kGreen-1]# ROOT.kOrange-1
     #analyses['SUS-24-003'] = ['results_sus_24_003/PureHiggsino_spdl_Run2comb_results.root', 'Exp_PureHiggsino_spdl_comb', 'Obs_PureHiggsino_spdl_comb', ROOT.kRed]
     analyses['Soft 2l and 3l'] = ['results_exo_23_017/h2lim_20250226_Hino_neg_allEEMM_neg_0.0_log_smooth1k5a_dMc1n1.root', 'limitGraph_0', 'limitGraph_obs', ROOT.kAzure-9]
-    cadis = list(analyses.keys())
-    cadis.reverse()
+    analysis_names = list(analyses.keys())
+    analysis_names.reverse()
     if doHLLHC:
-        for cadi in cadis:
-            analysis = analyses[cadi]
+        for analysis_name in analysis_names:
+            analysis = analyses[analysis_name]
             analysis[0] = analysis[0].replace('Run2','HLLHC')
             analysis[2] = ''
                 
@@ -40,18 +41,14 @@ def main():
     dummyExp.SetLineStyle(2)            # dashed
     dummyExp.SetLineColor(ROOT.kBlack)  # black line
     dummyExp.SetLineWidth(2)
-    #dummyExp.SetFillColor(ROOT.kYellow) # yellow fill
-    #dummyExp.SetFillStyle(1001)         # solid fill
-    legend00 = ROOT.TLegend(0.61, 0.45, 0.88, 0.60)
+    legend00 = ROOT.TLegend(0.61, 0.54, 0.88, 0.69)
     legend00.SetHeader("All limits at 95% CL", "L")  
-    #legend00.AddEntry(dummyObs, "Observed Limit (#pm 1#sigma^{SUSY}_{theory})", "l")
-    #legend00.AddEntry(dummyExp, "Expected Limit (#pm 1#sigma^{exp})", "lf")
     legend00.AddEntry(dummyObs, "Observed Limit", "l")
     legend00.AddEntry(dummyExp, "Expected Limit", "l")
     legend00.SetBorderSize(0)
     legend00.SetFillStyle(0)
     legend00.SetTextSize(0.03)
-    legend = ROOT.TLegend(0.61, 0.28-0.07*(len(analyses)-3), 0.88, 0.43) #0.53
+    legend = ROOT.TLegend(0.61, 0.37-0.035*(len(analyses)-2), 0.88, 0.51+0.035*(len(analyses)-2)) #0.53
     legend.SetBorderSize(0)
     legend.SetFillStyle(0)
     legend.SetTextSize(0.03)
@@ -71,8 +68,8 @@ def main():
     base_hist.GetXaxis().SetTitle('m_{#tilde{#chi}^{#pm}_{1}} [GeV]')
     base_hist.GetYaxis().SetTitle('#Deltam(#tilde{#chi}^{#pm}_{1},#tilde{#chi}^{0}_{1}) [GeV]')
     print(base_hist.GetXaxis().GetTitle(), base_hist.GetYaxis().GetTitle())
-    for cadi in cadis:
-        analysis = analyses[cadi]
+    for analysis_name in analysis_names:
+        analysis = analyses[analysis_name]
         path, exp, obj, color = analysis
         print('doing', analysis)
         file = ROOT.TFile.Open(path)
@@ -81,7 +78,7 @@ def main():
         gexp.SetLineStyle(ROOT.kDashed)
         gexp.SetLineWidth(3)
         mg.Add(gexp)            
-        #legend.AddEntry(gexp, "%s Expected" % cadi, "l")            
+        #legend.AddEntry(gexp, "%s Expected" % analysis_name, "l")            
         if obj=='': gobs = None
         else: 
             gobs = file.Get(obj)
@@ -90,8 +87,8 @@ def main():
             gobs.SetLineWidth(3)
             gobs.SetFillColorAlpha(color, 0.35)
             mg.Add(gobs, "LF")
-            #legend.AddEntry(gobs, "%s Observed" % cadi, "l")
-            legend.AddEntry(gobs, "%s" % cadi, "f")
+            #legend.AddEntry(gobs, "%s Observed" % analysis_name, "l")
+            legend.AddEntry(gobs, "%s" % analysis_name, "f")
         file.Close()
     canvas.cd()
     canvas.SetLogy()
@@ -102,12 +99,22 @@ def main():
     legend.Draw()
     #canvas.SetLogy()
     #canvas.SetGrid()
-    if doHLLHC: stamp(3000)
-    else: stamp()
+    if drawRadidativeBound:
+        fsatoshi = ROOT.TFile('auxiliary/SatoshiDmChipmChi10.root')
+        dmrad_chipm_chi10 = fsatoshi.Get('dmrad_chipm_chi10')
+        dmrad_chipm_chi10.SetLineColor(ROOT.kOrange+2)
+        dmrad_chipm_chi10.SetLineWidth(2)
+        dmrad_chipm_chi10.SetLineStyle(ROOT.kDashed)        
+        dmrad_chipm_chi10.Draw('same')
+        legend.AddEntry(dmrad_chipm_chi10, "Lower bound (theory)", "l")
+    
+    if doHLLHC: 
+        stamp(3000)
+    else: 
+        stamp()
     plotstem = 'summary_ewk_compressed'+'_HLLHC'*doHLLHC
     canvas.SaveAs(plotstem+".pdf")
     canvas.SaveAs(plotstem+".png")
-    print ("Summary plot saved as {plotstem}'.pdf'")
 
 def stamp(lumi='129-138', datamc_ = 'data', showlumi = True, WorkInProgress = False):
     cmsTextFont = 61
@@ -125,11 +132,11 @@ def stamp(lumi='129-138', datamc_ = 'data', showlumi = True, WorkInProgress = Fa
     tl.DrawLatex(0.12,0.91, 'CMS')
     tl.SetTextFont(extraTextFont)
     tl.SetTextSize(1.0/0.98*tl.GetTextSize())
-    xlab = 0.2
+    xlab = 0.201
     if ('mc' in datamc): thing = 'simulation'
     else: thing = 'preliminary'
     if WorkInProgress: tl.DrawLatex(xlab,0.91, ' internal')
-    else: tl.DrawLatex(xlab,0.91, thing)
+    else: tl.DrawLatex(xlab,0.912, thing)
     tl.SetTextFont(regularfont)
     tl.SetTextSize(0.81*tl.GetTextSize())    
     thingy = ''
